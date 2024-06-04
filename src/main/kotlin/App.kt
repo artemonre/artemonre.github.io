6c64e2bdbe28af997c2b8cf0483a4e86ad931163
Artem.Balzan <artem.balzan@e-legion.com>
2024-06-04 18:32:45 +0200

    Print1

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -6,7 +6,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import kotlinx.serialization.json.Json
 import presentation.BotDelegate
-import java.io.File
 
 const val nonWordCharOrStart = "([^а-яА-Я0-9a-zA-Z]|^)"
 const val nonWordCharOrEnd = "([^а-яА-Я0-9a-zA-Z]|$)"
@@ -15,7 +14,9 @@ const val nonWordCharOrEnd = "([^а-яА-Я0-9a-zA-Z]|$)"
  * This method by default expects one argument in [args] field: telegram bot configuration
  */
 suspend fun main(args: Array<String>) {
-    println(args)
+    args.forEach {
+        println("arg = $it")
+    }
     val json = Json { ignoreUnknownKeys = true }
     val config: Config = json.decodeFromString(Config.serializer(), args.first())
     val bot = telegramBot(config.token) {
